fdb22ba9b21459e8ccf07573d17f77d06ee5a02b
Alejo <106360950+Alejo-Aguirre@users.noreply.github.com>
2023-08-30 16:47:34 -0500

    funcionado pero toca hacer rerun del cliente para que procese las solicitudes ; se puede mejorar pero asi es funcional

--- src/main/java/co/uniquindio/practicas/model/EchoTCPClient.java
+++ src/main/java/co/uniquindio/practicas/model/EchoTCPClient.java
@@ -1,2 +1,103 @@
-package co.uniquindio.practicas.model;public class EchoTCPClient {
+package co.uniquindio.practicas.model;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+
+/**
+ * Alejo
+ * cliente practica 4
+ *
+ */
+public class EchoTCPClient {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static final String SERVER = "localhost";
+    public static final int PORT = 3400;
+
+    private PrintWriter toNetwork;
+    private BufferedReader fromNetwork;
+
+    private Socket clientSideSocket;
+
+    public EchoTCPClient() {
+        System.out.println("Echo TCP client is running ...");
+    }
+
+    public void init() throws Exception {
+        clientSideSocket = new Socket(SERVER, PORT);
+        createStreams(clientSideSocket);
+        menu(); // Llama al método menu() para mostrar el menú y gestionar la interacción.
+    }
+
+    private void createStreams(Socket socket) throws Exception {
+        toNetwork = new PrintWriter(socket.getOutputStream(), true);
+        fromNetwork = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public void menu() throws IOException {
+        int opc;
+        boolean exit = false;
+
+        do {
+            System.out.println("MENU:");
+            System.out.println("1. Ingresar usuario");
+            System.out.println("2. Mostrar informe de usuarios");
+            System.out.println("3. Mostrar informe detallado de usuarios");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese su opción: ");
+
+            opc = Integer.parseInt(SCANNER.nextLine());
+
+            switch (opc) {
+                case 1:
+                    System.out.print("Ingrese su nombre de usuario: ");
+                    String userName = SCANNER.nextLine();
+
+                    toNetwork.println(".LOGIN " + userName);
+
+                    try {
+                        String response = fromNetwork.readLine();
+                        System.out.println("[Server] " + response);
+                    } catch (IOException e) {
+                        System.err.println("Error al recibir la respuesta delservidor: " + e.getMessage());
+                    }
+                    break;
+                case 2:
+                    toNetwork.println(".INFORME");
+
+                    try {
+                        String response = fromNetwork.readLine();
+                        System.out.println("[Server] " + response);
+                    } catch (IOException e) {
+                        System.err.println("Error al recibir la respuesta del servidor: " + e.getMessage());
+                    }
+                    break;
+                case 3:
+                    toNetwork.println(".INFORME_DETALLADO");
+
+                    try {
+                        String response = fromNetwork.readLine();
+                        System.out.println("[Server] " + response);
+                    } catch (IOException e) {
+                        System.err.println("Error al recibir la respuesta del servidor: " + e.getMessage());
+                    }
+                    break;
+                case 4:
+                    System.out.println("Saliendo del cliente...");
+                    exit = true; // Establece la bandera de salida en verdadero para salir del bucle
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        } while (!exit);
+        clientSideSocket.close();
+    }
+
+    public static void main(String args[]) throws Exception {
+        EchoTCPClient ec = new EchoTCPClient();
+        ec.init();
+    }
 }
--- src/main/java/co/uniquindio/practicas/model/EchoTCPServer.java
+++ src/main/java/co/uniquindio/practicas/model/EchoTCPServer.java
@@ -1,2 +1,113 @@
-package co.uniquindio.practicas.model;public class EchoTCPServer {
+package co.uniquindio.practicas.model;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+
+/**
+ * Alejo,edisson,jonathan
+ * Servidor practica 4
+ *
+ */
+public class EchoTCPServer {
+    public static final int PORT = 3400;
+    private ServerSocket listener;
+    private Socket serverSideSocket;
+    private PrintWriter toNetwork;
+    private BufferedReader fromNetwork;
+
+    private HashMap<String, Integer> usersMap = new HashMap<>();
+    private int userCount = 0;
+
+    public EchoTCPServer() {
+        System.out.println("Echo TCP server is running on port: " + PORT);
+    }
+
+    public void init() throws Exception {
+        listener = new ServerSocket(PORT);
+        while (true) {
+            serverSideSocket = listener.accept();
+            createStreams(serverSideSocket);
+            protocol(serverSideSocket);
+        }
+    }
+
+    /**
+     * el protocolo incluye las solicitudes de los informes y  el split para los usuarios
+     * @param socket
+     * @throws Exception
+     */
+    public void protocol(Socket socket) throws Exception {
+        String message = fromNetwork.readLine();
+        System.out.println("[Server] From client: " + message);
+
+        if (message.startsWith(".LOGIN")) {
+            String[] parts = message.split(" ");
+            if (parts.length == 2) {
+                String userName = parts[1];
+                if (usersMap.containsKey(userName)) {
+                    int loginCount = usersMap.get(userName);
+                    loginCount++;
+                    usersMap.put(userName, loginCount);
+                    toNetwork.println(userName + ": Usted ha ingresado " + loginCount + " veces al sistema");
+                } else {
+                    userCount++;
+                    usersMap.put(userName, 1);
+                    toNetwork.println("BIENVENIDO " + userName + ", usted es el usuario #" + userCount);
+                }
+            } else {
+                toNetwork.println("Comando no válido");
+            }
+        } else if (message.equals(".INFORME")) {
+            sendUserList();
+        } else if (message.equals(".INFORME_DETALLADO")) {
+            sendDetailedUserList();
+        } else {
+            toNetwork.println("Comando no reconocido");
+        }
+    }
+
+    /**
+     * metodo para enviar la lista normal
+     */
+    private void sendUserList() {
+        StringBuilder userList = new StringBuilder();
+        for (String user : usersMap.keySet()) {
+            userList.append(user).append(", ");
+        }
+        if (userList.length() > 0) {
+            userList.setLength(userList.length() - 2);
+        }
+        toNetwork.println(userList.toString());
+        toNetwork.flush(); // Asegura que los datos se envíen al cliente
+
+        // No cierres la conexión aquí
+    }
+
+    /**
+     * metodo para enviar la lista detallada con el numero de ingresos de cada usuario
+     */
+    private void sendDetailedUserList() {
+        StringBuilder detailedUserList = new StringBuilder();
+        for (HashMap.Entry<String, Integer> entry : usersMap.entrySet()) {
+            detailedUserList.append(entry.getKey()).append(" ").append(entry.getValue()).append(", ");
+        }
+        if (detailedUserList.length() > 0) {
+            detailedUserList.setLength(detailedUserList.length() - 2);
+        }
+        toNetwork.println(detailedUserList.toString());
+        toNetwork.flush(); // Asegura que los datos se envíen al cliente
+
+        // No cierres la conexión aquí
+    }
+
+    private void createStreams(Socket socket) throws Exception {
+        toNetwork = new PrintWriter(socket.getOutputStream(), true);
+        fromNetwork = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static void main(String args[]) throws Exception {
+        EchoTCPServer es = new EchoTCPServer();
+        es.init();
+    }
 }
